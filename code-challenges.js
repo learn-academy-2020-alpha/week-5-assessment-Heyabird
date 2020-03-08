5a3c8e764d859e39c41a4aa9a9a814ef927d1bf8
Heya Kwon <heyabird@gmail.com>
2020-03-07 17:15:32 -0800

    submitting first try

--- code-challenges.js
+++ code-challenges.js
@@ -2,7 +2,24 @@
 
 // --------------------1) Create a function that takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.
 
-
+const coder = (string) => {
+    arr1 = string.split("")
+    arr2 = arr1.map(value =>  {
+        if (value=='a') {
+            return 4
+        } else if (value=='e') {
+            return 3
+        } else if (value=='i') {
+            return 1
+        } else if (value=='o') {
+            return 0
+        } else {
+            return value
+        }
+    }
+    )
+    return arr2.join("")
+}
 
 
 
@@ -11,22 +28,53 @@ var secretCodeWord1 = "lackadaisical"
 var secretCodeWord2 = "gobbledygook"
 // Expected output: "g0bbl3dyg00k"
 
+//TESTING
+console.log(coder(secretCodeWord1))
+console.log(coder(secretCodeWord2))
+
 // --------------------2) Create a function that takes in an array and returns all the words that have the letter a in them.
 
-
+const ayo = (array) => {
+    newArr = array.filter(value => value.includes("a") || value.includes("A"))
+    return newArr.join(" ")
+}
 
 
 
 var arrayOfWords = ["Apple", "Banana", "Plum", "Cherry", "Kiwi", "Peach"]
 // Expected output: "Apple" "Banana" "Peach"
 
-
+console.log(ayo(arrayOfWords))
 
 
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is one pair and one three of a kind.
 
-
+fullHouse = (array) => {
+    //make count variables
+    count0=0
+    count1=0
+    //find out the unique values
+    uniqueNum = [...new Set(array)]
+    // console.log(uniqueNum)
+    if (uniqueNum.length == 2) {
+        //count how many times uniqueNum[0] and uniqueNum[1] appear in the OG array 
+        for(let i=0; i<array.length; i++) {
+            if (array[i] === uniqueNum[0]) {
+                count0+=1
+            } else if (array[i] === uniqueNum[1]) {
+                count1+=1
+            }
+        }
+        //check if the count values are 2 and 3 -- if so, return true
+        if (count0==2 && count1==3 || count0==3 && count1==2) {
+        return true
+        }
+    //otherwise, return false
+    } else {
+        return false
+    }
+    }
 
 
 
@@ -34,3 +82,6 @@ var hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
 var hand2 = [5, 5, 3, 3, 4]
 // Expected output: false
+
+console.log("hand1:", fullHouse(hand1))
+console.log("hand2:", fullHouse(hand2))
--- rails-questions.md
+++ rails-questions.md
@@ -2,26 +2,32 @@
 
 1. What is the command to create a new Rails application with a Postgres database?
 
-
+rails new app_name -d postgresql -T
 
 2. Explain each section of the following route:  get '/library/:id' => 'book#show'
 
-get -
-library -
-:id -
-book -
-show -
+get - an http verb we use to order the server to retreive what is in the url
+library - url
+:id - param
+book - name of controller
+show - name of method
 
 3. Oops, I forgot to add a foreign key to my model. Describe the steps to remedy this mistake.
 
-
+use migrate: 
+- execute 'rails generate migration add_foreign_key_column' in terminal
+- inside the migration folder, find the most recent migration file. inside the def change function, put in 'add_column :model_plural, :model_id, :integer' (switch 'model' into the actual model name)
+- execute 'rails db:migrate' in terminal
 
 
 
 4. What is the command to generate a Rails model for a person table with columns named shirt, pants, and shoes?
 
-
+rails generate model Person shirt:integer pants:integer shoes:integer
 
 
 
 5. I want to add validations to the model from question 4 to ensure no one can make a new entry without data in all the columns. How do I do that?
+
+inside the person.rb file, and inside the Person class, put in:
+- 'validates :shirt, :pants, :shoes, presence: true'
